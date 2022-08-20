034b739b57f51ce064374c57cc26ad3165ba10d1
Vladimir Ovchinnikov <metalass21@yandex.ru>
2022-08-21 00:42:44 +0300

    feat: сверстать страницу аутентификации и добавить auth.js в utils

---
+++ src/components/AuthPage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function AuthPage({
+  formName,
+  onSubmit,
+  title,
+  btnText,
+  children,
+}) {
+  return (
+    <div className="auth-page">
+      <form
+        className="auth-page__form"
+        name={formName}
+        noValidate
+        onSubmit={onSubmit}
+      >
+        <h2 className="auth-page__title">{title}</h2>
+        {children}
+        <button type="submit" className="auth-page__btn">
+          {btnText}
+        </button>
+        {formName === 'register' && (
+          <Link className="auth-page__link" to="/sign-up">
+            Уже зарегистрированы? Войти
+          </Link>
+        )}
+      </form>
+    </div>
+  );
+}
